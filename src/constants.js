4f8afb1c387aa0a6a4902cf6900c4941968b8876
Daniel Indlekofer <daniel@indlekofer.org>
2019-11-10 09:38:04 +0100

    v1.1.1

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-const module = '@indlekofer/ajax_json/ERROR_';
+const name = '@indlekofer/ajax_json/ERROR_';
 
-export const ERROR_TIMEOUT = module + 'TIMEOUT';
-export const ERROR_JSON = module + 'JSON';
-export const ERROR_REJECT = module + 'REJECT';
+export const ERROR_TIMEOUT = name + 'TIMEOUT';
+export const ERROR_JSON = name + 'JSON';
+export const ERROR_REJECT = name + 'REJECT';
